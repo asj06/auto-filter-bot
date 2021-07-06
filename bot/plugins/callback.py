9ad1117c75b75fd5469d51be06691b0defcc9788
Avishkar Patil <avishkarpatil29@gmail.com>
2021-07-06 09:06:17 +0530

    1

--- bot/plugins/callback.py
+++ bot/plugins/callback.py
@@ -1600,15 +1600,14 @@ async def cb_about(bot, update: CallbackQuery):
 
     text=f"<i><u>Bot's Status</u></i>\n"
     text+=f"\n<b><i>Bot's Uptime:</i></b> <code>{time_formatter(time.time() - start_uptime)}</code>\n"
-    text+=f"\n<b><i>Bot Funtion:</i></b> <i>Auto Filter Files</i>\n"
-    text+=f"""\n<b><i>Bot Support:</i></b> <a href="https://t.me/CrazyBotszGrp">@CrazyBotszGrp</a>\n"""
-    text+="""\n<b><i>Source Code:</i></b> <a href="https://github.com/CrazyBotsz/Adv-Filter-Bot-V2">Source</a>"""
-
+    text+=f"\n<b><i>Bot Funtion:</i></b> <i>StreamingFreaks ka Sardar</i>\n"
+    text+=f"""\n<b><i>Father:</i></b> <a href="https://t.me/amogh_06">@Amogh_06</a>\n"""
+    
     buttons = [
         [
             InlineKeyboardButton
                 (
-                    "My Dev ⚡", url="https://t.me/AlbertEinstein_TG"
+                    "Follow 🎃", url="https://GitHub.com/avipatilpro"
                 ),
                 
             InlineKeyboardButton
@@ -1639,12 +1638,10 @@ async def callback_data(bot, update: CallbackQuery):
 
     if query_data == "start":
         buttons = [[
-            InlineKeyboardButton('My Dev 👨‍🔬', url='https://t.me/AlbertEinstein_TG'),
-            InlineKeyboardButton('Source Code 🧾', url ='https://github.com/CrazyBotsz/Adv-Filter-Bot-V2')
-        ],[
-            InlineKeyboardButton('Support 🛠', url='https://t.me/CrazyBotszGrp')
-        ],[
-            InlineKeyboardButton('Help ⚙', callback_data="help")
+            InlineKeyboardButton('Boss 😎', url='https://t.me/amogh_06'),
+            InlineKeyboardButton('StreamingFreaks 😈', url='https://t.me/StreamingFreaks'),
+        
+            InlineKeyboardButton('Contact 🎀', callback_data="help")
         ]]
     
         reply_markup = InlineKeyboardMarkup(buttons)
@@ -1707,4 +1704,3 @@ def time_formatter(seconds: float) -> str:
         ((str(minutes) + "m, ") if minutes else "") + \
         ((str(seconds) + "s") if seconds else "")
     return tmp
-
